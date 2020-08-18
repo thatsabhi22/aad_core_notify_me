4592881e0674f20b923473d13b538e186ffccac4
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-08-19 03:50:15 +0530

    Intiail Commit

---
+++ app/src/main/java/com/theleafapps/pro/notifyme/MainActivity.java
@@ -0,0 +1,51 @@
+package com.theleafapps.pro.notifyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    private Button button_notify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    public void sendNotification() {}
+
+    public void createNotificationChannel()
+    {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(PRIMARY_CHANNEL_ID,
+                            "Zandu Notification",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification for Sample");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+}
